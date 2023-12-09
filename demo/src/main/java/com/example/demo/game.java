294b45c653ec151e2adb68e85e6c436664ce22be
Vaibhav sehara <85846845+vaibhavsehara@users.noreply.github.com>
2023-12-09 19:15:19 +0530

    Update game.java
    fuckkk yesssss
    it worksssss

--- demo/src/main/java/com/example/demo/game.java
+++ demo/src/main/java/com/example/demo/game.java
@@ -2,18 +2,18 @@ package com.example.demo;
 
 import javafx.animation.*;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Point3D;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Line;
 import javafx.animation.TranslateTransition;
 import javafx.scene.shape.LineTo;
@@ -30,12 +30,13 @@ public class game extends Application {
     private double count = 0;
     private double Score = 0;
     private ImageView character;
+    private ImageView ScoreSigil;
     private double completedStickLength = 0;
     private Timeline stickTimeline;
     Platform platform1 = new Platform(300, 500, 200, 500); // Increased width of the platform
     Platform platform2 = new Platform(100, 500, 700, 500);
     Platform platform3;
-    private Pane root;
+    private AnchorPane root;
     private boolean Inverted = false;
     private Line createdStick;
     private boolean isMoving = false;
@@ -58,7 +59,7 @@ public class game extends Application {
     private Scene scene = createGameScene();
     private boolean isCharacterMoving = false;
     private double gap;
-
+    private boolean characterCollided = false;
     private Scene createGameScene( javafx.stage.Stage primaryStage){
 
         return new Scene(root, 1600, 800);
@@ -66,7 +67,24 @@ public class game extends Application {
 
     @Override
     public void start(javafx.stage.Stage primaryStage) {
-        Sigil = new ImageView(new Image("Sigil.jpeg"));
+        ScoreSigil = new ImageView(new Image("anemo.png"));
+        ScoreSigil.setFitHeight(30);
+        ScoreSigil.setFitWidth(30);
+        Label scoreLabel = new Label("Score: 0");
+        scoreLabel.setStyle("-fx-font-size: 24; -fx-text-fill: #011627;"); // adjust the style as needed
+
+// Position the ScoreSigil and scoreLabel at the top right
+        HBox scoreBox = new HBox(ScoreSigil, scoreLabel);
+        scoreBox.setAlignment(Pos.TOP_RIGHT);
+
+// Add the scoreBox to the root pane
+
+
+// Update the scoreLabel text whenever the score changes
+// This should be done in your game loop or wherever you update the score
+
+
+        Sigil = new ImageView(new Image("anemo.png"));
         Sigil.setFitHeight(20);
         Sigil.setFitWidth(20);
 
@@ -79,7 +97,7 @@ public class game extends Application {
 
         Image logoIcon = new Image("snake.jpg");
 
-        platform3 = new Platform(300, 500, 1200, 500);
+        platform3 = new Platform(300, 400, 1200, 500);
         // Load the video file
         Image gameBg = new Image("mondstat.jpeg");
         ImageView gameBgImage = new ImageView(gameBg);
@@ -87,17 +105,17 @@ public class game extends Application {
         gameBgImage.setFitHeight(800);
         gameBgImage.setPreserveRatio(false);
 
-        characterImage = new Image("/character.png");
+        characterImage = new Image("Lumine.png");
 
         // Set the initial position of the character
         defaultStickManX = platform1.getX() + platform1.getWidth();
         character = new ImageView(characterImage);
         character.setFitWidth(50);
-        character.setFitHeight(100);
+        character.setFitHeight(70);
         character.setTranslateX(defaultStickManX-50);
-        character.setTranslateY(410);
+        character.setTranslateY(435);
         translateTransition = new TranslateTransition(Duration.millis(400), character);
-        root = new Pane();
+        root = new AnchorPane();
 
         // Create a StackPane and add the character (ImageView) and media view to it
         StackPane stackPane = new StackPane();
@@ -112,6 +130,11 @@ public class game extends Application {
         root.getChildren().add(stackPane);
         Sigil.setY(platform1.getY() + 10);
         root.getChildren().add(button);
+        root.getChildren().add(scoreBox);
+        AnchorPane.setTopAnchor(scoreBox, 10.0); // 10 units down from the top edge
+        AnchorPane.setRightAnchor(scoreBox, 10.0);
+
+        scoreLabel.setText("Score: " + Score);
         // Add platform rectangles to the root
         root.getChildren().add(platform1.getRectangle());
         root.getChildren().add(platform2.getRectangle());
@@ -141,10 +164,10 @@ public class game extends Application {
             if (event.getCode() == KeyCode.I){
                 if(!Inverted && isCharacterBetweenGap()){
                character.setRotate(180);
-               character.setLayoutY(character.getLayoutY()+80);
+               character.setLayoutY(character.getLayoutY()+60);
             }else if (Inverted && isCharacterBetweenGap()){
                     character.setRotate(0);
-                    character.setLayoutY(character.getLayoutY()-80);
+                    character.setLayoutY(character.getLayoutY()-60);
                 }
                 Inverted = !Inverted;
             }
@@ -205,7 +228,7 @@ public class game extends Application {
     private void reset() {
         // Reset the character's position
         character.setTranslateX(defaultStickManX);
-        character.setTranslateY(500); // Assuming 350 is the initial Y position of the character
+        character.setTranslateY(400); // Assuming 350 is the initial Y position of the character
 
         // Allow the creation of a new stick
         canCreateStick = true;
@@ -221,8 +244,8 @@ public class game extends Application {
         root.getChildren().remove(platform3.getRectangle());
         root.getChildren().remove(platform2.getRectangle());
 
-        platform1 = new Platform(300, 500, 200, 500); // Increased width of the platform
-        platform2 = new Platform(300, 500, 700, 500);
+        platform1 = new Platform(300, 400, 200, 500); // Increased width of the platform
+        platform2 = new Platform(300, 400, 700, 500);
 
 
     }
@@ -340,7 +363,8 @@ public class game extends Application {
                 isMoving = false;
                 isCharacterAtEndPoint = true;
                 isCharacterMoved = true;
-                moveAllComponents();
+                if( !characterCollided){
+                moveAllComponents();}
                 double updatedX = platform1.getX();
                 double updatedY = platform1.getY();
                 canCreateStick = true;
@@ -359,6 +383,7 @@ public class game extends Application {
                 if (completedStickLength >= platformDistance && completedStickLength <= distanceToEndOfNextPlatform) {
                     double endX = platform2.getX() + platform2.getWidth() - 60;
                     double endY = character.getTranslateY();
+
                     moveCharacter(endX, endY);
                 } else {
                     fallCharacter();
@@ -369,6 +394,49 @@ public class game extends Application {
             // Step 5: If the character has collected the sigil, increase the score and remove the sigil from the root pane
             Score++; // assuming you have a score variable
             root.getChildren().remove(Sigil);}
+        if (character.getBoundsInParent().intersects((platform2.getRectangle().getBoundsInParent())) && Inverted) {
+            if (character.getTranslateX() + character.getFitWidth() >= platform2.getX()) {
+                StopCharacter();
+                fallCharacterVertically();
+                characterCollided = true;
+            }
+        }
+    }
+    private void StopCharacter() {
+        if (translateTransition != null) {
+            translateTransition.stop();
+        }
+    }
+    private void fallCharacterVertically() {
+        // Set the character's X position to the leftmost edge of the platform
+        character.setTranslateX(platform2.getX());
+
+        // Calculate the distance to fall
+        double distance = root.getHeight() - character.getTranslateY();
+
+        // Calculate the time it takes for the character to fall
+        double time = 2; // Adjust the speed as needed
+
+        // Create a KeyValue for the falling action
+        KeyValue fallKeyValue = new KeyValue(character.translateYProperty(), character.getTranslateY() + distance);
+
+        // Create a KeyFrame for the falling action
+        KeyFrame fallKeyFrame = new KeyFrame(Duration.seconds(time), fallKeyValue);
+
+        // Create a Timeline for the falling action
+        Timeline fallTimeline = new Timeline(fallKeyFrame);
+
+        fallTimeline.setOnFinished(fallEvent -> {
+            // Remove the character from the root pane once it has fallen
+            root.getChildren().remove(character);
+
+            // Show the retry screen
+            if (root.getScene().getWindow() instanceof javafx.stage.Stage) {
+                showRetryScreen((javafx.stage.Stage) root.getScene().getWindow());
+            }
+        });
+
+        fallTimeline.play();
     }
     private void moveAllComponents() {
         if (!isMoving) {
@@ -376,7 +444,6 @@ public class game extends Application {
 
             // Calculate the distance to move
             double distance = platform2.getX();
-            System.out.println(distance);
 
             // Calculate the time it takes for the character to complete its journey
             double time = 1; // Adjust the speed as needed
@@ -399,31 +466,51 @@ public class game extends Application {
             TranslateTransition stickTransition = new TranslateTransition(Duration.seconds(time), currentStickLine);
             stickTransition.setByX(-distance);
             stickTransition.play();
-
-
+            System.out.println(platform1.getX());
+            System.out.println(platform2.getX());
             // Create and play the transition for platform1
             TranslateTransition platform1Transition = new TranslateTransition(Duration.seconds(time), platform1.getRectangle());
             platform1Transition.setByX(-distance);
             platform1Transition.setOnFinished(event -> {
+
                 isMoving = false;
                 isComponentsMoved = true;
+
+                // Update the position of the sigil and the start of the stick based on the new positions of the platforms
+//                double gapStart = platform1.getX() + platform1.getWidth();
+//                double gapEnd = platform2.getX();
+//                double gapMiddle = (gapStart + gapEnd) / 2;
+//                Sigil.setX(gapMiddle);
+//                Sigil.setY(platform1.getY() + 10);
+//                root.getChildren().add(Sigil);
+                platform1.setX(platform2.getX() - gap-300);
+                System.out.println(platform1.getX());
+                if (currentStickLine != null) {
+                    currentStickLine.setStartX(platform1.getX() + platform1.getWidth());
+                    currentStickLine.setStartY(platform1.getY());
+                }
+                System.out.println(platform1.getX());
+
             });
             platform1Transition.play();
-            System.out.println("Platform X: " + platform1.getX());
-            System.out.println("Platform Y: " + platform1.getY());
-
 
             // Create and play the transition for platform2
             TranslateTransition platform2Transition = new TranslateTransition(Duration.seconds(time), platform2.getRectangle());
             platform2Transition.setByX(-distance);
+            platform2Transition.setOnFinished(event ->{
+//                platform2.setX(platform2.getX()- distance);
+                System.out.println(platform2.getX());
+            });
             platform2Transition.play();
 
             // Create and play the transition for platform3
             TranslateTransition platform3Transition = new TranslateTransition(Duration.seconds(time), platform3.getRectangle());
-            platform3Transition.setByX(-distance);
+//            platform3Transition.setByX(-distance);
+
             platform3Transition.play();
         }
     }
+
 
 
     private void switchPlatform() {
@@ -441,10 +528,23 @@ public class game extends Application {
         if (isComponentsMoved) {
             canCreateStick = true;
         }
+        Inverted = false;
         isSpaceBarLocked = false;
 
         // Create a new stick at the right edge of the new platform
-
+        resetAllBoolean();
+    }
+    private void resetAllBoolean() {
+        isMoving = false;
+        isCharacterAtEndPoint = false;
+        isSpaceBarLocked = false;
+        canCreateStick = true;
+        isStickCreated = false;
+        isCharacterMoved = false;
+        isComponentsMoved = false;
+        Inverted = false;
+        isSpaceBarPressed = false;
+        characterCollided = false;
     }
 
     private void showRetryScreen(javafx.stage.Stage primaryStage) {
@@ -490,7 +590,7 @@ public class game extends Application {
         double time = 2; // Adjust the speed as needed
 
         // Create a KeyValue for the moving action
-        KeyValue moveKeyValue = new KeyValue(character.translateXProperty(), character.getTranslateX() + completedStickLength + 60);
+        KeyValue moveKeyValue = new KeyValue(character.translateXProperty(), character.getTranslateX() + completedStickLength +40);
         // Create a KeyFrame for the moving action
         KeyFrame moveKeyFrame = new KeyFrame(Duration.seconds(time), moveKeyValue);
 
@@ -534,14 +634,31 @@ public class game extends Application {
         if (platform1 != null) {
             root.getChildren().remove(platform1.getRectangle());
         }
+        if (Sigil != null) {
+            root.getChildren().remove(Sigil);
+        }
 
         // Move the current platform to the position of the old platform
         platform1 = platform2;
 
         // Generate a new platform with random width and position
-        double gap = 50 + Math.random() * 450;
+        double randomOffset = Math.random() * 100;
+        gap = 50 + Math.random() * 300;
+        System.out.println("gap = " + gap);
         double wid = 100 + Math.random() * 200;
         platform2 = new Platform(wid, 500, 300+ + gap, 500);
+
+        Sigil = new ImageView(new Image("anemo.png"));
+        Sigil.setFitHeight(20);
+        Sigil.setFitWidth(20);
+
+        double gapStart = platform1.getX() + platform1.getWidth();
+        double gapEnd = platform2.getX();
+        double gapMiddle = gapEnd -10 - randomOffset;
+        Sigil.setX(gapMiddle);
+        Sigil.setY(platform1.getY() + 10);
+
+        root.getChildren().add(Sigil);
 
         // Add the new platform to the root
         root.getChildren().add(platform2.getRectangle());
@@ -561,7 +678,7 @@ public class game extends Application {
         bgImageView.setPreserveRatio(false);
 
         // Load the image and create an ImageView object
-        Image playImage = new Image("play.jpeg"); // Replace "play.png" with your image file
+        Image playImage = new Image("START.png"); // Replace "play.png" with your image file
         ImageView playImageView = new ImageView(playImage);
 
         // Set an onMouseClicked event to the ImageView
